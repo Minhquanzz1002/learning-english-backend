227deddd84c4aa6a8e0500d60ae4e570fe39a9a4
Nguyễn Minh Quân <quannguyenminh1001@gmail.com>
2024-07-16 04:02:18 +0700

    feat: update topic (#1)

--- src/api/v1/controllers/topic.controller.ts
+++ src/api/v1/controllers/topic.controller.ts
@@ -6,19 +6,34 @@ import mongoose from 'mongoose';
 import { BadRequestError } from '../errors/BadRequestError';
 
 const createTopic = async (req: express.Request, res: express.Response, next: NextFunction) => {
-  const {name, description} = req.body;
-  const existingTopic = await Topic.findOne({name: name});
+  const { name, description } = req.body;
+  const existingTopic = await Topic.findOne({ name: name });
   if (existingTopic) {
     return next(new BadRequestError(`Tên chủ đề đã tồn tại`));
   }
-  const topic = new Topic({name, description});
+  const topic = new Topic({ name, description });
   const savedTopic = await topic.save();
   return res.status(201).send(savedTopic);
 };
 
-const updateTopic = (req: express.Request, res: express.Response) => {
-  Logger.info(res);
-  Logger.info(req);
+const updateTopic = async (req: express.Request, res: express.Response, next: NextFunction) => {
+  const { name, description } = req.body;
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new NotFoundError('Không tìm thấy đề mục'));
+  }
+  const topic = await Topic.findOne({ _id: id, status: 'ACTIVE' });
+  if (!topic) {
+    return next(new NotFoundError('Không tìm thấy đề mục'));
+  }
+  if (name) {
+    topic.name = name;
+  }
+  if (description) {
+    topic.description = description;
+  }
+  const updatedTopic = await topic.save();
+  return res.send(updatedTopic);
 };
 
 const deleteTopic = (req: express.Request, res: express.Response) => {
@@ -29,20 +44,20 @@ const deleteTopic = (req: express.Request, res: express.Response) => {
 const getTopic = async (req: express.Request, res: express.Response, next: NextFunction) => {
   const { id } = req.params;
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return next(new NotFoundError("Không tìm thấy đề mục"));
+    return next(new NotFoundError('Không tìm thấy đề mục'));
   }
-  const topic = await Topic.findOne({_id: id, status: 'ACTIVE'});
+  const topic = await Topic.findOne({ _id: id, status: 'ACTIVE' });
   if (!topic) {
-    return next(new NotFoundError("Không tìm thấy đề mục"));
+    return next(new NotFoundError('Không tìm thấy đề mục'));
   }
   return res.send(topic);
 };
 
 const getTopics = async (req: express.Request, res: express.Response) => {
-  const {name} = req.query;
+  const { name } = req.query;
   const filter = {
     status: 'ACTIVE',
-    ...(name && {name: {$regex: name, $options: 'i'}})
+    ...(name && { name: { $regex: name, $options: 'i' } }),
   };
   const topics = await Topic.find(filter);
   return res.send(topics);
