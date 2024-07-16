d6f1ffb0ad188c21209535f780aff119c65b5f51
Nguyễn Minh Quân <quannguyenminh1001@gmail.com>
2024-07-17 02:32:46 +0700

    feat: vocabulary api (#1)

--- src/routes/v1/topic.router.ts
+++ src/routes/v1/topic.router.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createTopic, getTopic, getTopics, updateTopic } from '../../api/v1/controllers/topic.controller';
+import { createVocabulary, getVocabularies, updateVocabulary } from '../../api/v1/controllers/vocabylary.controller';
 
 const topicRouter = express.Router();
 
@@ -7,5 +8,9 @@ topicRouter.post('/', createTopic);
 topicRouter.get('/', getTopics);
 topicRouter.get('/:id', getTopic);
 topicRouter.put('/:id', updateTopic);
+topicRouter.post('/:topicId/vocabularies', createVocabulary);
+topicRouter.get('/:topicId/vocabularies', getVocabularies);
+topicRouter.put('/:topicId/vocabularies/:vocabularyId', updateVocabulary);
+
 
 export default topicRouter;
